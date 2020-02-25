744cbbd86243256f171721e1b6d540eea6734935
zwyak <vlad.senyuk97@gmail.com>
2020-02-25 15:36:39 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer  = require('multer');
+const path    = require('path');
 
 const storage = multer.diskStorage({
   destination: './uploads/',
@@ -8,13 +9,28 @@ const storage = multer.diskStorage({
   }
 });
 
+const pdfStorage = multer.diskStorage({
+  destination: './uploads/pdf/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
 const upload = multer({ storage: storage });
+const pdf = multer({ storage: pdfStorage });
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('file'), (req, res, next) => {
+  res.json({ succeed: true });
+});
+
+app.post('/pdf', pdf.single('file'), (req, res, next) => {
+  if (path.extension(file.originalname) == '.pdf') {
+
+  }
   res.json({ succeed: true });
 });
 
