da8b8df7ea1df030eec3e3d176e8b9dc3ff88142
zwyak <vlad.senyuk97@gmail.com>
2020-03-05 17:52:42 +0300

    Added image uploader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
-const path = require('path');
+const path =    require('path');
+const sharp =   require('sharp');
 
 const storage = multer.diskStorage({
   destination: './uploads/',
@@ -13,6 +14,24 @@ const pdfStorage = multer.diskStorage({
   destination: './uploads/pdf/',
   filename: function (req, file, cb) {
     cb(null, Date.now() + '.pdf');
+  }
+});
+
+const imageStorage = multer.diskStorage({
+  destination: './uploads/images/',
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+
+    sharp(file)
+    .resize(800, 600)
+    .then( data => { cb(null, 'preview' + Date.now() + ext); })
+    .catch( err => { console.error(err);});
+
+    sharp(file)
+    .resize(300, 180)
+    .then( data => { cb(null, 'thumbnail' + Date.now() + ext); })
+    .catch( err => { console.error(err);});
   }
 });
 
@@ -31,9 +50,23 @@ const pdf = multer(
     }
 });
 
+const image = multer(
+  {
+    storage: imageStorage,
+    fileFilter: (req, file, cb) => {
+      const ext = path.extname(file.originalname);
+      if(ext !== '.png' && ext != '.jpg') {
+        return cb(null, false);
+      }else{
+        cb(null, true)
+      }
+    }
+});
+
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('uploads/images'));
 
 app.post('/upload', upload.single('file'), (req, res, next) => {
   res.json({ succeed: true });
@@ -45,6 +78,10 @@ app.post('/pdf', pdf.array('files', 3), (req, res, next) => {
       return;
     }
     res.json({ succeed: true, files: req.files });
+});
+
+app.post('/images', image.single('image'), (req, res, next) => {
+    res.json({ images: req.file });
 });
 
 app.listen(3000, () => console.log('Server app listening on port 3000!'));
